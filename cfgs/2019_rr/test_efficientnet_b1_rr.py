c5bc9650cdc463df96aaa5055457bd7302ab4575
PeterKampen <s183993@student.dtu.dk>
2021-03-09 10:34:38 +0100

    input size

--- cfgs/2019_rr/test_efficientnet_b1_rr.py
+++ cfgs/2019_rr/test_efficientnet_b1_rr.py
@@ -30,7 +30,7 @@ def init(mdlParams_):
     mdlParams['classification'] = True
     mdlParams['balance_classes'] = 9
     mdlParams['extra_fac'] = 1.0
-    mdlParams['numClasses'] = 9
+    mdlParams['numClasses'] = 8
     mdlParams['no_c9_eval'] = True
     mdlParams['numOut'] = mdlParams['numClasses']
     mdlParams['numCV'] = 1
@@ -45,7 +45,7 @@ def init(mdlParams_):
     mdlParams['multiCropEval'] = mdlParams['numCropPositions']*len(mdlParams['cropScales'])*mdlParams['cropFlipping']
     mdlParams['offset_crop'] = 0.2
     # Scale up for b1-b7
-    mdlParams['input_size'] = [224,224,3]
+    mdlParams['input_size'] = [240, 240, 3]
 
     ### Training Parameters ###
     # Batch size
