1f89e738386c6ae688a1c27648392883128a44c4
PeterKampen <s183993@student.dtu.dk>
2021-03-12 11:23:23 +0100

    Intermediate_results

--- pickle_load.py
+++ pickle_load.py
@@ -1,8 +1,7 @@
 import pickle
 import pandas as pd
 
-p = pickle.load(open(r'C:\Users\Bruger\Desktop\CV.pkl', "rb"))
-panda_p = pd.read_pickle(r'C:\Users\Bruger\Desktop\CV.pkl')
-f = open("dict.txt","w")
-f.write( str(panda_p) )
-f.close
+pcl = pickle.load(open(r'C:\Users\ptrkm\OneDrive\Dokumenter\Bachelor deep learning\2019.test_efficientnet_b1_ss\CV.pkl', "rb"))
+print(pcl.keys())
+breakpoint()
+
