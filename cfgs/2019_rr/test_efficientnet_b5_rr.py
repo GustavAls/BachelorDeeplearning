26f6495ffa57fb2ffeb8bad033758cfa726a69d8
Gustav Als <gustavals1998@gmail.com>
2021-03-19 16:22:25 +0100

    our final ensemble trained on all data

--- cfgs/2019_rr/test_efficientnet_b5_rr.py
+++ cfgs/2019_rr/test_efficientnet_b5_rr.py
@@ -49,7 +49,7 @@ def init(mdlParams_):
 
     ### Training Parameters ###
     # Batch size
-    mdlParams['batchSize'] = 8#*len(mdlParams['numGPUs'])
+    mdlParams['batchSize'] = 7#*len(mdlParams['numGPUs'])
     # Initial learning rate
     mdlParams['learning_rate'] = 0.000015#*len(mdlParams['numGPUs'])
     # Lower learning rate after no improvement over 100 epochs
